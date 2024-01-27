34c3a093a47070d49902e5e26efd210ef1738b1e
EsraAbdirashid <israApdirashid@gmail.com>
2024-01-27 06:11:35 +0300

    Done

--- vite-project/src/components/Singup.jsx
+++ vite-project/src/components/Singup.jsx
@@ -9,11 +9,11 @@ const Singup = () => {
     <form action="" className='mt-7'>
         <h2 className='text-white font-extrabold'>SignUp</h2>
        
-      {/* <input type="text"placeholder='User Name' required className='mt-8 px-[100px] rounded-md shadow-md shadow-white hover:border-2 outline-none p-2 '/>
+      <input type="text"placeholder='User Name' required className='mt-8 px-[100px] rounded-md shadow-md shadow-white hover:border-2 outline-none p-2 '/>
       <i class='bx bx-user -translate-x-7 text-orange-600' ></i> <br />
       <input type="text" placeholder='Email or Phone' required className='mt-8 px-[100px] rounded-md outline-none p-2 shadow-md shadow-white hover:border-2 ' />  
       <i class='bx bxs-user-plus -translate-x-7 text-orange-600'></i> <br />
-      <input type="password"placeholder='********' required id='password'  className='mt-8 px-[100px]  rounded-md outline-none p-2 shadow-md hover:border-2 shadow-white'/> */}
+      <input type="password"placeholder='********' required id='password'  className='mt-8 px-[100px]  rounded-md outline-none p-2 shadow-md hover:border-2 shadow-white'/>
       <input type="checkbox" id='showHide' className='p-2 -translate-x-6' /> <br /> <br /><br />
 
       <input type="submit" placeholder='Send' className='border-2
